8472f4ffd011de8fc3e2e75542af5e16350fcf5b
Chris <krzysztof.kobylinski1@gmail.com>
2021-06-24 21:00:35 +0200

    HomePage with pagination

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,10 +1,53 @@
-import React from 'react';
-import { withRouter } from "react-router-dom";
+import React, {useEffect, useState} from 'react';
 
-function Homepage() {
+import axios from "axios";
+import SmallEntry from "../redactor_entries/SmallEntry";
+import Typography from "@material-ui/core/Typography";
+import Pagination from '@material-ui/lab/Pagination';
+
+const Homepage = () => {
+    const [entries, setEntries] = useState([]);
+    const [pageNr, setPageNr] = useState(0);
+    const [pageCount, setPageCount] = useState(10);
+    const displayedEntries = 5;
+
+    useEffect( () => {
+       getPage(pageNr);
+    }, []);
+
+    const getPage = (pageNr) => {
+        axios.get(
+            `http://localhost:8081/api/blog/getFrontPage?page=${pageNr - 1}&size=${displayedEntries}`,
+        ).then((response) => {
+            if(response.status === 200) {
+                setEntries(response.data.content)
+                if(pageNr >= pageCount && response.data.content.length === displayedEntries ) {
+                    setPageCount(pageNr + 1);
+                }
+            }
+        }).catch(function (error) {
+            console.log(error);
+        });
+    }
+
+    const changePage = (event, value) => {
+        setPageNr(value);
+        getPage(value);
+    }
+
     return(
         <div>
+            <div>
+                {entries.map((entry) =>
+                        <SmallEntry key={entry.createdDate} entry ={entry}/>
+                    )}
+            </div>
+            <Typography>Page: {pageNr}</Typography>
+            <Pagination count={pageCount} page={pageNr} size="large"
+                        variant="outlined" shape="rounded"
+                        onChange={changePage} color="secondary" />
         </div>
+
     )
 }
-export default withRouter(Homepage);
+export default Homepage;
